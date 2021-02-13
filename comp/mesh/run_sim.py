ee2d15dbd0fdc9bd376ee0a94c887331e0d4457b
emmilner <emma.milner@bristol.ac.uk>
2021-02-13 16:45:58 +0000

    mesh changes mainly to aggregate behaviours and to call out for help if stuck behind lots of boxes

--- comp/mesh/run_sim.py
+++ comp/mesh/run_sim.py
@@ -7,16 +7,18 @@ print("simulating task 1 with bias")
 'Inputs'
 task = 1 # Task 1 or 2?
 bias = 1 # Should the behaviour have a heading bias or not?
-swarm_size = 50 #umber of agents in the swarm 
+swarm_size = 10 #umber of agents in the swarm 
 num_items = 50 #mber of items to be collected
-time_limit = 2000 # how long to give the robots to complete the task
-ani = True # Do you want to generate an animation of the behaviour?
+time_limit = 20000 # how long to give the robots to complete the task
+ani = False # Do you want to generate an animation of the behaviour?
 ##################################################
 if task == 1:
 	if bias == 0:
 		sim = sim_task_1.data(swarm_size,num_items,ani,time_limit)
 	if bias == 1:
-		sim = sim_task_1_bias.data(swarm_size,num_items,ani,time_limit)
+		print("swarm size", swarm_size)
+		for i in range(10):
+			sim = sim_task_1_bias.data(swarm_size,num_items,ani,time_limit)
 
 if task == 2:
 	if bias == 0:
